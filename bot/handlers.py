b56e3f8ae9bf0a60c6ab0cccb6ff1b9f2cece47f
Osama-Is <37333584+Osama-Is@users.noreply.github.com>
2023-08-05 11:43:03 +0300

    Update Requirements.txt

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,29 +1,51 @@
-from telethon import events
+from enum import Enum, auto
+
+from telethon import events, Button
+
 from utils.function_refs import make_ref
-import boto3
-import random
-import time
+from .models import flow_steps, Student
+from .enums import States
 
 
 # Obtain the handlers' references to be passed to client.add_event_handler in the main.py file.
 handlers = []
 
 
+
 @make_ref(handlers)
 @events.register(events.NewMessage(pattern='/start'))
-async def handle_start_command(event):
-    # Respond with "bot is running" when the user sends "/start"
-    result = await event.respond("bot is running")
-    print(result)
+async def start_handler(event):
+    user_id = event.sender_id
+    
+    # await event.respond(
+    #     "حياكم الله، يمكنكم إرسال الأسئلة إلى اللجنة العلمية عن طريق هذا البوت، تفضل باختيار القسم الذي يتعلق به السؤال.",
+    #     buttons=[
+    #         [create_inline_button("القسم الشرعي", "option_1"), create_inline_button("القسم الثقافي", "option_2")]
+    #     ]
+    # )
 
-# @events.register(events.Raw)
-# async def handle_start_command(event):
-#     # Respond with "bot is running" when the user sends "/start"
-#     # await event.respond("bot is running")
-#     print(event)
-#id=6699
+
 
 @make_ref(handlers)
-@events.register(events.NewMessage)
-async def handle_forwarded_messages(event):
-    await event.respond(f'next number is: {random.randint(0, 99999999)}')
+@events.register(events.NewMessage(incoming=True))
+async def message_handler(event):
+    pass
+
+
+@make_ref(handlers)
+@events.register(events.CallbackQuery(pattern=b"sub_sub_option_\d"))
+async def sub_sub_option_handler(event):
+    user_id = event.sender_id
+    selected_option = event.data.decode()
+
+    # if selected_option == 'sub_sub_option_1':
+    #     await event.edit(
+    #         "من فضلك قم بإرسال اسم السلسلة"
+    #     )
+    #     user_states[user_id] = 4
+    # elif selected_option == 'sub_sub_option_2':
+    #     await event.edit(
+    #         "من فضلك قم بإرسال اسم الكتاب"
+    #     )
+    #     user_states[user_id] = 5
+    # raise events.StopPropagation
--- utils/function_refs.py
+++ utils/function_refs.py
@@ -1,10 +1,38 @@
 from typing import List, Callable
 
-
 def make_ref(refs_list: List[Callable] = []) -> Callable:
+    """
+    Decorator factory to create a decorator that tracks references to decorated functions.
+
+    Parameters:
+        refs_list (List[Callable], optional): A list to store references to decorated functions.
+            If not provided, it defaults to an empty list.
+
+    Returns:
+        decorator (Callable): A decorator function that can be used to decorate other functions.
+            The decorator adds the reference of the decorated function to the `refs_list` and
+            then returns the original function without any modification.
+
+    Example:
+        @make_ref(referenced_functions)
+        def foo():
+            return "Hello, world!"
+
+        @make_ref(referenced_functions)
+        def bar():
+            return "This is another function."
+
+        # At this point, referenced_functions will contain references to the decorated functions foo and bar.
+
+        # You can call the decorated functions as usual:
+        result_foo = foo()
+        result_bar = bar()
+
+    Note:
+        The `make_ref` decorator factory can be used in various scenarios where you need to track
+        references to decorated functions, such as logging, event handling, or other custom use cases.
+    """
     def decorator(func: Callable) -> Callable:
-        # Add the function reference to the refs_list
         refs_list.append(func)
-        # Return the original function
         return func
     return decorator
